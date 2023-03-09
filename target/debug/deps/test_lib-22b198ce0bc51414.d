d5a44410273123640a4ca1bc91d6845ad703333e
RobinDBHW <thyeria@gmx.de>
2023-03-09 12:25:07 +0100

    second test

---
+++ target/debug/deps/test_lib-22b198ce0bc51414.d
@@ -0,0 +1,5 @@
+/Users/robin-bahr/test-lib/target/debug/deps/test_lib-22b198ce0bc51414.rmeta: src/lib.rs
+
+/Users/robin-bahr/test-lib/target/debug/deps/test_lib-22b198ce0bc51414.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/debug/deps/test_lib-4d9c31a4ee38f996.d
@@ -0,0 +1,5 @@
+/Users/robin-bahr/test-lib/target/debug/deps/test_lib-4d9c31a4ee38f996.rmeta: src/lib.rs
+
+/Users/robin-bahr/test-lib/target/debug/deps/test_lib-4d9c31a4ee38f996.d: src/lib.rs
+
+src/lib.rs:
